11364128e9f5d74f5c04d30659d248b07c3a4524
Alisa Modeste <user@alisamodeste.com>
2013-12-11 10:00:26 -0500

    Working on table

--- app/assets/javascripts/numbers.js
+++ app/assets/javascripts/numbers.js
@@ -1,3 +1,3 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+//on submit send ajax for day, year, min, hour, month
+
+//$("#body-cells").html()

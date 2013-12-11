41a97ee723075e5237b2cbc22230979130d88b79
Alisa Modeste <user@alisamodeste.com>
2013-12-11 17:58:06 -0500

    Working on getting the date range

--- app/assets/javascripts/numbers.js
+++ app/assets/javascripts/numbers.js
@@ -6,8 +6,9 @@
 	var NumberTracker = root.NumberTracker = (root.NumberTracker || {} );
 
 	var ending = NumberTracker.ending = new Date();
-	var beginning = NumberTracker.beginning = ending - 
-	var increment = NumberTracker.increment = "day"
+	var beginning = NumberTracker.beginning = ending - 9;
+	var interval = NumberTracker.interval = "day";
+	var addition = NumberTracker.addition = 12;
 
 
 	var changeRange = NumberTracker.changeRange = function(next){
@@ -18,7 +19,7 @@
 				startDate: beginning,
 				endDate: ending,
 				timezoneOffset: ending.getTimezoneOffset(),
-				increment: increment
+				interval: interval
 			},
 			error: function(){
 
@@ -32,8 +33,11 @@
 
 	//var twelveHourFormat = NumberTracker.twelveHourFormat = function(hour, min, sec){
 	var twelveHourFormat = NumberTracker.twelveHourFormat = function(timestamp){
+		var date = new Date(timestamp);
+		var hour = date.getHours(), min = date.getMinutes(), sec = date.getSeconds();
+
 		if (hour < "12"){
-			hour = 12 if hour == "0";
+			if (hour == "0") hour = 12 ;
 			return [hour,min,sec].join(":") + " AM";
 		}
 		else {
@@ -43,32 +47,73 @@
 
 	};
 
-	var getPeriodEnd = NumberTracker.getPeriodEnd = function(number){
-		if (number.created_at.min == 59){
-			if (number.created_at.hour == 23){
-				return twelveHourFormat("00:00:00")
+	var getPeriodEnd = NumberTracker.getPeriodEnd = function(timestamp){
+		var date = new Date(timestamp);
+		var hour = date.getHours(), min = date.getMinutes(), sec = date.getSeconds();
+
+		if (min == 59){
+			if (hour == 23){
+				return "12:00:00 AM";
 			}
 			else {
-				return twelveHourFormat( (number.created_at.hour +1) +":00:00")
+				return (hour + 1) +":00:00";
 			}
 		}
 		else {
-			return twelveHourFormat( (number.created_at.hour) +":"+ (number.created_at.min+1) +":00")
+			return twelveHourFormat( (number.created_at.hour) +":"+ (number.created_at.min+1) +":00");
 		}
-	}
+	};
+
+	var getPeriodStart = NumberTracker.getPeriodStart = function(timestamp){
+		var date = new Date(timestamp);
+		var hour = date.getHours(), min = date.getMinutes(), sec = date.getSeconds();
+		var yr = date.getFullYear(), day = date.getDate(), month = date.getMonth+1
+
+		switch(interval){
+			case min:
+			//hour:min A/P - hour:min+1 A/P
+
+
+			case hour:
+			//HH:00 - HH+1:00
+
+			case day:
+			//Month Day - Month Day+1
+			moment().add('days', addition)._d;
+
+			case month:
+			//Month - Month+1
+
+
+			case year:
+			//Year - Year +1
+		}
+
+		if (min == 59){
+			if (hour == 23){
+				return "12:00:00 AM";
+			}
+			else {
+				return (hour + 1) +":00:00";
+			}
+		}
+		else {
+			return twelveHourFormat( (number.created_at.hour) +":"+ (number.created_at.min+1) +":00");
+		}
+	};
 
 
 	var populateTable = NumberTracker.populateTable = function(numbers){
 		$("body-cells").empty();
 
-		@numbers.forEach(number){
-		$("body-cells").append("<tr>");
-			$("body-cells").append("<td>"+twelveHourFormat(number.created_at.hour, number.created_at.min, 0) +"</td>");
-			$("body-cells").append("<td>"+getPeriodEnd(number) %> </td>
-			<td><%#= number.number %></td>
-		</tr>
-		}
-	}
+		numbers.forEach(function(number){
+			$("body-cells").append("<tr>");
+			$("body-cells").append("<td>"+twelveHourFormat(number.timestamp) +"</td>");
+		// 	$("body-cells").append("<td>"+getPeriodEnd(number.timestamp) %> </td>
+		// 	<td><%#= number.number %></td>
+		// </tr>
+		});
+	};
 
 
 })(window);

5ba4145c861284166ff0ab24c3a125094cc795e9
Alisa Modeste <user@alisamodeste.com>
2013-12-11 14:12:35 -0500

    Moving date conversion to client side

--- app/assets/javascripts/numbers.js
+++ app/assets/javascripts/numbers.js
@@ -5,22 +5,60 @@
 (function (root){
 	var NumberTracker = root.NumberTracker = (root.NumberTracker || {} );
 
-	var beginning = NumberTracker.beginning =
 	var ending = NumberTracker.ending = Time.now();
+	var beginning = NumberTracker.beginning = ending - 
+	var increment = NumberTracker.increment = "day"
 
 
-	$.ajax({
-		data: {
-			previous: true,
-			startDate: "12/12/12",
-			endDate: "12/18/12"
-		},
-		error: function(){
+	var changeRange = NumberTracker.changeRange = function(next){
 
-		},
-		success: function(){
+		$.ajax({
+			data: {
+				next: next,
+				startDate: beginning,
+				endDate: ending,
+				timezoneOffset: ending.getTimezoneOffset(),
+				increment: increment
+			},
+			error: function(){
 
+			},
+			success: function(){
+
+			}
+		});
+	};
+
+
+	var twelveHourFormat = NumberTracker.twelveHourFormat = function(time){
+		time = time.split(":")
+
+		if (time.first < "12"){
+			time[0] = 12 if time.first == "0";
+			time.join(":") + " AM";
 		}
-	})
+		else {
+			time[0] = time.first.to_i % 12;
+			time.join(":") + " PM";
+		}
+
+		return time;
+	};
+
+	var getPeriodEnd = NumberTracker.getPeriodEnd = function(number){
+		if (number.created_at.min == 59){
+			if (number.created_at.hour == 23){
+				twelve_hour_format("00:00:00")
+			}
+			else {
+				twelve_hour_format( (number.created_at.hour +1) +":00:00")
+			}
+		}
+		else {
+			twelve_hour_format( (number.created_at.hour) +":"+ (number.created_at.min+1) +":00")
+		}
+	}
+
+
 
 })(window);
--- app/models/number.rb
+++ app/models/number.rb
@@ -1,33 +1,33 @@
 class Number < ActiveRecord::Base
   attr_accessible :number
 
-  def self.get_period_end(number)
-		if number.created_at.min == 59
-			if number.created_at.hour == 23
-				twelve_hour_format("00:00:00")
-			else
-				twelve_hour_format("#{number.created_at.hour +1}:00:00")
-			end
-		else
-			twelve_hour_format("#{number.created_at.hour}:#{number.created_at.min+1}:00")
-		end
-	end
+ #  def self.get_period_end(number)
+	# 	if number.created_at.min == 59
+	# 		if number.created_at.hour == 23
+	# 			twelve_hour_format("00:00:00")
+	# 		else
+	# 			twelve_hour_format("#{number.created_at.hour +1}:00:00")
+	# 		end
+	# 	else
+	# 		twelve_hour_format("#{number.created_at.hour}:#{number.created_at.min+1}:00")
+	# 	end
+	# end
 
 
-	def self.twelve_hour_format(time)
-		time = time.split(":")
-		# if time.first == 00
-		# 	time.first = 12
-		# elsif time.first < 12
+	# def self.twelve_hour_format(time)
+	# 	time = time.split(":")
+	# 	# if time.first == 00
+	# 	# 	time.first = 12
+	# 	# elsif time.first < 12
 
-		# time.first = time.first % 12
+	# 	# time.first = time.first % 12
 
-		if time.first < "12"
-			time[0] = 12 if time.first == "0"
-			time.join(":") + " AM"
-		else 
-			time[0] = time.first.to_i % 12
-			time.join(":") + " PM"		
-		end
-	end
+	# 	if time.first < "12"
+	# 		time[0] = 12 if time.first == "0"
+	# 		time.join(":") + " AM"
+	# 	else 
+	# 		time[0] = time.first.to_i % 12
+	# 		time.join(":") + " PM"		
+	# 	end
+	# end
 end

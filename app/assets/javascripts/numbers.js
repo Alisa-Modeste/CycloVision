e408074fa7bb2d26c516bc1429ebd19c63547e1f
Alisa Modeste <user@alisamodeste.com>
2013-12-11 14:41:01 -0500

    Changing because server will send timestamp

--- app/assets/javascripts/numbers.js
+++ app/assets/javascripts/numbers.js
@@ -5,7 +5,7 @@
 (function (root){
 	var NumberTracker = root.NumberTracker = (root.NumberTracker || {} );
 
-	var ending = NumberTracker.ending = Time.now();
+	var ending = NumberTracker.ending = new Date();
 	var beginning = NumberTracker.beginning = ending - 
 	var increment = NumberTracker.increment = "day"
 
@@ -30,35 +30,45 @@
 	};
 
 
-	var twelveHourFormat = NumberTracker.twelveHourFormat = function(time){
-		time = time.split(":")
-
-		if (time.first < "12"){
-			time[0] = 12 if time.first == "0";
-			time.join(":") + " AM";
+	//var twelveHourFormat = NumberTracker.twelveHourFormat = function(hour, min, sec){
+	var twelveHourFormat = NumberTracker.twelveHourFormat = function(timestamp){
+		if (hour < "12"){
+			hour = 12 if hour == "0";
+			return [hour,min,sec].join(":") + " AM";
 		}
 		else {
-			time[0] = time.first.to_i % 12;
-			time.join(":") + " PM";
+			hour = hour % 12;
+			return [hour,min,sec].join(":") + " PM";
 		}
 
-		return time;
 	};
 
 	var getPeriodEnd = NumberTracker.getPeriodEnd = function(number){
 		if (number.created_at.min == 59){
 			if (number.created_at.hour == 23){
-				twelve_hour_format("00:00:00")
+				return twelveHourFormat("00:00:00")
 			}
 			else {
-				twelve_hour_format( (number.created_at.hour +1) +":00:00")
+				return twelveHourFormat( (number.created_at.hour +1) +":00:00")
 			}
 		}
 		else {
-			twelve_hour_format( (number.created_at.hour) +":"+ (number.created_at.min+1) +":00")
+			return twelveHourFormat( (number.created_at.hour) +":"+ (number.created_at.min+1) +":00")
 		}
 	}
 
+
+	var populateTable = NumberTracker.populateTable = function(numbers){
+		$("body-cells").empty();
+
+		@numbers.forEach(number){
+		$("body-cells").append("<tr>");
+			$("body-cells").append("<td>"+twelveHourFormat(number.created_at.hour, number.created_at.min, 0) +"</td>");
+			$("body-cells").append("<td>"+getPeriodEnd(number) %> </td>
+			<td><%#= number.number %></td>
+		</tr>
+		}
+	}
 
 
 })(window);
